bdef8773fbdf2628630cd925c86379b475b2ec60
Ray Yamamoto Hilton <ray@wirestorm.net>
2023-04-03 21:44:43 +1000

    More subtle

--- src/components/ParticleEffect.tsx
+++ src/components/ParticleEffect.tsx
@@ -1,11 +1,8 @@
 import { useCallback } from "react";
 import Particles from "react-particles";
 import { loadFull } from "tsparticles";
-// import { parallax } from "tsparticles-demo-configs";
 import type { Container, Engine } from "tsparticles-engine";
-
-const parallax = {"name":"Parallax","particles":{"number":{"value":100,"density":{"enable":true}},"color":{"value":["#00ff33","#ff3300","#0033ff"]},"shape":{"type":"circle"},"opacity":{"value":{"min":0.1,"max":0.5},"animation":{"enable":false}},"size":{"value":2,"animation":{"enable":false}},"links":{"enable":true,"distance":300,"color":"#777777","opacity":0.4,"width":1},"move":{"enable":true,"speed":2}},"interactivity":{"events":{"onHover":{"enable":false,"mode":"grab","parallax":{"enable":true,"smooth":10,"force":100}},"onClick":{"enable":true,"mode":"push"}},"modes":{"grab":{"distance":400,"links":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":0.8},"repulse":{"distance":200},"push":{"quantity":4},"remove":{"quantity":2}}},"background":{"color":"#000022"},"fullScreen":{"enable":true,"zIndex":-1}}
-console.log(JSON.stringify(parallax))
+import { parallaxConfig } from "./parallax";
 
 export const ParticleEffect = () => {
     const particlesInit = useCallback(async (engine: Engine) => {
@@ -22,6 +19,6 @@ export const ParticleEffect = () => {
     }, []);
 
     return (
-        <Particles id="tsparticles" init={particlesInit} loaded={particlesLoaded} options={parallax} />
+        <Particles id="tsparticles" init={particlesInit} loaded={particlesLoaded} options={parallaxConfig} />
     );
 };
